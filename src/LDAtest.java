2263c3c68ad4179dc4a52ec6acfdc1954bcc4d95
aranyali <aranyaelendil@gmail.com>
2015-03-01 00:31:51 -0800

    combine NER and LDA with some bugs

---
+++ src/LDAtest.java
@@ -0,0 +1,163 @@
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Formatter;
+import java.util.Locale;
+import java.util.Scanner;
+import java.util.TreeSet;
+import java.util.regex.Pattern;
+
+import opennlp.tools.namefind.NameFinderME;
+import opennlp.tools.namefind.TokenNameFinderModel;
+import opennlp.tools.util.InvalidFormatException;
+import opennlp.tools.util.Span;
+
+import cc.mallet.grmm.inference.MessageArray.Iterator;
+import cc.mallet.pipe.CharSequence2TokenSequence;
+import cc.mallet.pipe.CharSequenceLowercase;
+import cc.mallet.pipe.Pipe;
+import cc.mallet.pipe.SerialPipes;
+import cc.mallet.pipe.TokenSequence2FeatureSequence;
+import cc.mallet.pipe.TokenSequenceRemoveStopwords;
+import cc.mallet.pipe.iterator.CsvIterator;
+import cc.mallet.topics.ParallelTopicModel;
+import cc.mallet.topics.TopicInferencer;
+import cc.mallet.types.Alphabet;
+import cc.mallet.types.FeatureSequence;
+import cc.mallet.types.IDSorter;
+import cc.mallet.types.Instance;
+import cc.mallet.types.InstanceList;
+import cc.mallet.types.LabelSequence;
+
+
+public class LDAtest {
+	public static void preprocess() throws InvalidFormatException, IOException{
+
+		InputStream modelIn = new FileInputStream("./models/en-ner-location.bin");
+		  TokenNameFinderModel model = new TokenNameFinderModel(modelIn);
+		  NameFinderME nameFinder = new NameFinderME(model);
+		  Scanner in=new Scanner(new FileReader("testdata"));
+		  PrintWriter out = new PrintWriter("ner.input", "UTF-8");
+		  int i=1;
+		  while(in.hasNextLine()){
+			  String cur=in.nextLine();
+			  String[] curs=cur.split(" ");
+			  Span nameE[]= nameFinder.find(curs);
+			  cur=Arrays.toString(Span.spansToStrings(nameE, curs));
+			  out.println(i+" "+"#"+" "+cur.substring(1, cur.length()-1));
+			  i++;
+		  }
+		  modelIn.close();
+		  out.close();
+	}
+	
+	public static InstanceList initInstance(Reader fileReader){
+		// Begin by importing documents from text to feature sequences
+        ArrayList<Pipe> pipeList = new ArrayList<Pipe>();
+
+        // Pipes: lowercase, tokenize, remove stopwords, map to features
+        pipeList.add( new CharSequenceLowercase() );
+        pipeList.add( new CharSequence2TokenSequence(Pattern.compile("\\p{L}[\\p{L}\\p{P}]+\\p{L}")) );
+        pipeList.add( new TokenSequenceRemoveStopwords(new File("models/stoplists/en.txt"), "UTF-8", false, false, false) );
+        pipeList.add( new TokenSequence2FeatureSequence() );
+       
+        InstanceList instances = new InstanceList (new SerialPipes(pipeList));
+
+        
+        instances.addThruPipe(new CsvIterator (fileReader, Pattern.compile("^(\\S*)[\\s,]*(\\S*)[\\s,]*(.*)$"),
+                                               3, 2, 1)); // data, label, name fields
+        
+        return instances;
+	}
+
+	public static void main(String[] args) throws Exception {
+		// read documents
+		preprocess();
+		Reader fileReader = new InputStreamReader(new FileInputStream(new File("ner.input")), "UTF-8");
+        InstanceList instances=initInstance(fileReader);
+        
+        // Create a model with 100 topics, alpha_t = 0.01, beta_w = 0.01
+        //  Note that the first parameter is passed as the sum over topics, while
+        //  the second is the parameter for a single dimension of the Dirichlet prior.
+        int numTopics = 10;
+        ParallelTopicModel model = new ParallelTopicModel(numTopics, 1.0, 0.01);
+
+        model.addInstances(instances);
+
+        // Use two parallel samplers, which each look at one half the corpus and combine
+        //  statistics after every iteration.
+        model.setNumThreads(2);
+
+        // Run the model for 50 iterations and stop (this is for testing only, 
+        //  for real applications, use 1000 to 2000 iterations)
+        model.setNumIterations(50);
+        model.estimate();
+
+        // Show the words and topics in the first instance
+
+        // The data alphabet maps word IDs to strings
+        Alphabet dataAlphabet = instances.getDataAlphabet();
+        
+        FeatureSequence tokens = (FeatureSequence) model.getData().get(0).instance.getData();
+        LabelSequence topics = model.getData().get(0).topicSequence;
+        
+        Formatter out = new Formatter(new StringBuilder(), Locale.US);
+        for (int position = 0; position < tokens.getLength(); position++) {
+            out.format("%s-%d ", dataAlphabet.lookupObject(tokens.getIndexAtPosition(position)), topics.getIndexAtPosition(position));
+        }
+        System.out.println(out);
+        
+        // Estimate the topic distribution of the first instance, 
+        //  given the current Gibbs state.
+        double[] topicDistribution = model.getTopicProbabilities(0);
+
+        // Get an array of sorted sets of word ID/count pairs
+        ArrayList<TreeSet<IDSorter>> topicSortedWords = model.getSortedWords();
+        
+        // Show top 5 words in topics with proportions for the first document
+        for (int topic = 0; topic < numTopics; topic++) {
+            java.util.Iterator<IDSorter> iterator = topicSortedWords.get(topic).iterator();
+            
+            out = new Formatter(new StringBuilder(), Locale.US);
+            out.format("%d\t%.3f\t", topic, topicDistribution[topic]);
+            int rank = 0;
+            while (iterator.hasNext() && rank < 5) {
+                Object idCountPair = iterator.next();
+                out.format("%s (%.0f) ", dataAlphabet.lookupObject(((IDSorter) idCountPair).getID()), ((IDSorter) idCountPair).getWeight());
+                rank++;
+            }
+            System.out.println(out);
+        }
+        
+        // Create a new instance with high probability of topic 0
+        StringBuilder topicZeroText = new StringBuilder();
+        java.util.Iterator<IDSorter> iterator = topicSortedWords.get(0).iterator();
+
+        int rank = 0;
+        while (iterator.hasNext() && rank < 5) {
+            IDSorter idCountPair = iterator.next();
+            topicZeroText.append(dataAlphabet.lookupObject(idCountPair.getID()) + " ");
+            rank++;
+        }
+
+        // Create a new instance named "test instance" with empty target and source fields.
+        InstanceList testing = new InstanceList(instances.getPipe());
+        testing.addThruPipe(new Instance(topicZeroText.toString(), null, "test instance", null));
+
+        TopicInferencer inferencer = model.getInferencer();
+        double[] testProbabilities = inferencer.getSampledDistribution(testing.get(0), 10, 1, 5);
+        System.out.println("0\t" + testProbabilities[0]);
+    }
+	
+}
